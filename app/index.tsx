f60f9c340c0afdb9c0dd77870f10f1d3365a4616
Michele Guidetti <contact@tkstudio.io>
2025-10-05 21:29:48 +0200

    feat: handle no profiles

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,14 @@
 import AppLayout from "@/components/layout/app";
 import { ProfilesList } from "@/components/profiles-list";
-import { Button, ButtonText } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
 import { VStack } from "@/components/ui/vstack";
-import { useRouter } from "expo-router";
 
 import React from "react";
 
 const Home = () => {
-  const router = useRouter();
   return (
     <AppLayout>
       <VStack space='4xl' className='items-center justify-center h-full'>
-        <Heading size='2xl'>Select profile</Heading>
         <ProfilesList />
-        <Button
-          onPress={() => router.replace("/create-profile")}
-          variant={"link"}
-        >
-          <ButtonText>Create new profile</ButtonText>
-        </Button>
       </VStack>
     </AppLayout>
   );
