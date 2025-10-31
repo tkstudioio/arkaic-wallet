efaf822e30b6800e4abfeb99536840d18373d47c
Michele Guidetti <contact@tkstudio.io>
2025-10-31 10:35:52 +0100

    chore: dashboard code

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,6 +1,8 @@
 import { AccountsCarousel } from "@/components/accounts-carousel";
 import AppLayout from "@/components/layout/app-layout";
 import { Transactions } from "@/components/transactions";
+import { Card } from "@/components/ui/card";
+import { Spinner } from "@/components/ui/spinner";
 import { VStack } from "@/components/ui/vstack";
 import useProfileStore from "@/stores/profile";
 
@@ -9,14 +11,19 @@ import React from "react";
 const DashboardPage = () => {
   const { wallet, showTransactionsList } = useProfileStore();
 
+  if (!wallet) return;
   return (
     <AppLayout>
-      {wallet ? <AccountsCarousel /> : null}
-      {showTransactionsList && wallet ? (
-        <VStack className='px-6'>
+      <AccountsCarousel />
+      <VStack className='px-6'>
+        {showTransactionsList ? (
           <Transactions />
-        </VStack>
-      ) : null}
+        ) : (
+          <Card>
+            <Spinner />
+          </Card>
+        )}
+      </VStack>
     </AppLayout>
   );
 };
