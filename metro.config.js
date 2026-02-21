730a98ea63a7fb7732da2229c3b99aadda7ef72d
Michele Guidetti <contact@tkstudio.io>
2026-02-21 15:15:19 +0100

    chore: configure metro bundler for SVG assets
    - Add react-native-svg-transformer for SVG support
    - Register SVG as source extension instead of asset extension
    - Enable SVG imports in JavaScript modules

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,16 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+config.resolver = {
+  ...config.resolver,
+  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...config.resolver.sourceExts, "svg"],
+};
+module.exports = withNativeWind(config, { input: "./global.css" });
