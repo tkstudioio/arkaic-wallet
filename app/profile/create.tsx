eeab620f12272683999a2773b37e1406cfd10859
Michele Guidetti <contact@tkstudio.io>
2026-02-21 15:16:00 +0100

    refactor(pages/profile): enhance profile creation page layout
    - Add account-info SVG illustration to profile creation page
    - Wrap form with heading and descriptive text
    - Improve visual hierarchy with VStack layout
    - Make form more visually cohesive with illustrations

--- app/profile/create.tsx
+++ app/profile/create.tsx
@@ -1,10 +1,29 @@
 import CreateOrRestoreProfileForm from "@/components/create-profile-form";
 import AuthLayout from "@/components/layout/auth-layout";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { VStack } from "@/components/ui/vstack";
+import { Image } from "expo-image";
+import { Dimensions } from "react-native";
 
 export default function ProfileCreatePage() {
   return (
     <AuthLayout>
-      <CreateOrRestoreProfileForm />
+      <VStack className='items-center' space={"4xl"}>
+        <Image
+          source={require("@/assets/images/account-info.svg")}
+          style={{
+            margin: "auto",
+            width: Dimensions.get("window").width * 0.8,
+            height: Dimensions.get("window").height * 0.2,
+          }}
+        />
+        <VStack className='items-center'>
+          <Heading>Account info</Heading>
+          <Text>Give your account a name and select a ASP to connect to</Text>
+        </VStack>
+        <CreateOrRestoreProfileForm />
+      </VStack>
     </AuthLayout>
   );
 }
