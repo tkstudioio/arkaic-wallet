304400bd5faffd18731c1fe1db1e4ce7aaf20aac
Michele Guidetti <contact@tkstudio.io>
2026-02-23 21:48:43 +0100

    fix(ui): restore send and receive action buttons on dashboard

--- app/dashboard.tsx
+++ app/dashboard.tsx
@@ -1,7 +1,10 @@
 import { AccountBalance } from "@/components/account-balance";
 import AppLayout from "@/components/layout/app-layout";
+import { ReceiveActionSheet } from "@/components/receive-action-sheet";
+import { SendActionSheet } from "@/components/send-action-sheet";
 import { Transactions } from "@/components/transactions";
 import { Card } from "@/components/ui/card";
+import { HStack } from "@/components/ui/hstack";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
 import useProfileStore from "@/stores/profile";
@@ -21,6 +24,10 @@ const DashboardPage = () => {
             <Card className='flex-1 w-full aspect-video items-center justify-center'>
               <AccountBalance profile={profile} />
             </Card>
+            <HStack space={"xl"} className='justify-center'>
+              <ReceiveActionSheet />
+              <SendActionSheet />
+            </HStack>
             <Transactions />
           </VStack>
         ))}
