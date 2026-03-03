52f52e7e650b2ff6623b16b185f22facc97e0a31
Michele Guidetti <contact@tkstudio.io>
2026-03-03 12:59:05 +0100

    refactor(account-balance): replace ts-pattern match with explicit conditionals and add account name badge

--- components/account-balance.tsx
+++ components/account-balance.tsx
@@ -3,8 +3,9 @@ import { useBalance } from "@/hooks/use-balance";
 import { useWallet } from "@/hooks/use-wallet";
 import { ArkaicAccount } from "@/types/arkaic";
 
-import { match } from "ts-pattern";
+import useAccountStore from "@/stores/account";
 import { AmountComponent } from "./amount";
+import { Badge, BadgeText } from "./ui/badge";
 import { HStack } from "./ui/hstack";
 import { Spinner } from "./ui/spinner";
 import { Large, P } from "./ui/typography";
@@ -12,33 +13,33 @@ import { VStack } from "./ui/vstack";
 
 export function AccountBalance(props: { account: ArkaicAccount }) {
   const { data: wallet } = useWallet(props.account);
+  const { account } = useAccountStore();
   const balanceQuery = useBalance(wallet);
 
-  return match(balanceQuery)
-    .with(
-      { isFetching: true },
-      { isLoading: true },
-      { isPending: true },
-      () => (
-        <HStack className='py-4 justify-center'>
-          <Spinner />
-        </HStack>
-      ),
-    )
-    .with({ isSuccess: true }, ({ data }) => (
-      <HStack className='py-4 justify-center'>
-        <AmountComponent amount={data?.available} size='6xl' />
-      </HStack>
-    ))
-    .otherwise(({ error }) => (
+  if (!balanceQuery.isFetched) return <Spinner />;
+
+  if (!balanceQuery.isSuccess)
+    return (
       <VStack className='items-center' space={"md"}>
         <VStack className='items-center'>
           <Large>Failed to fetch balance</Large>
           <P>
-            {error?.message} {error?.stack}
+            {balanceQuery.error?.message} {balanceQuery.error?.stack}
           </P>
-          <P>{error?.stack}</P>
+          <P>{balanceQuery.error?.stack}</P>
         </VStack>
       </VStack>
-    ));
+    );
+
+  return (
+    <VStack className='items-center pb-12 pt-6' space={"sm"}>
+      <Badge size={"xl"}>
+        <BadgeText>{account?.name}</BadgeText>
+      </Badge>
+      <HStack>
+        <Spinner className='absolute -right-4 ' />
+        <AmountComponent amount={balanceQuery.data?.available} size='6xl' />
+      </HStack>
+    </VStack>
+  );
 }
