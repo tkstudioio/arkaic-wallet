446da08f8efadd9b1a99e24fbdb419a635959995
Michele Guidetti <contact@tkstudio.io>
2026-03-07 19:56:10 +0100

    feat(products): add product pages scaffolding
    Add empty shell pages for product listing and creation
    wrapped with PublicLayout.

---
+++ app/products/create.tsx
@@ -0,0 +1,5 @@
+import PublicLayout from "@/components/layouts/public-layout";
+
+export default function ProductCreate() {
+  return <PublicLayout></PublicLayout>;
+}
---
+++ app/products/list.tsx
@@ -0,0 +1,5 @@
+import PublicLayout from "@/components/layouts/public-layout";
+
+export default function ProductsList() {
+  return <PublicLayout></PublicLayout>;
+}
