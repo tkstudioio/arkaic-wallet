890e13038a3945d02e64f1520f3436af7d92b939
Michele Guidetti <contact@tkstudio.io>
2026-03-13 07:14:49 +0100

    feat(api): add auth header, schnorr signature, and auth route bypass to request interceptor

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,6 @@
+import useAccountStore from "@/stores/account";
+import { schnorr } from "@noble/curves/secp256k1";
+import { hex } from "@scure/base";
 import axios, { AxiosError } from "axios";
 
 export const API_BASE_URL = "http://localhost:4000/v2";
@@ -7,6 +10,30 @@ export const backend = axios.create({
 });
 
 backend.interceptors.request.use(async (config) => {
+  const { getState } = useAccountStore;
+
+  const { token, account } = getState();
+
+  const isAuthRoute = config.url?.startsWith("/auth/");
+
+  if (!token || isAuthRoute) return config;
+
+  config.headers.Authorization = `Bearer ${token}`;
+
+  const method = config.method?.toUpperCase();
+
+  if (method === "POST" || method === "PUT" || method === "PATCH") {
+    const payload = new TextEncoder().encode(JSON.stringify(config.data));
+
+    if (!account?.privateKey) return config;
+
+    const signature = hex.encode(
+      schnorr.sign(payload, hex.decode(account?.privateKey)),
+    );
+
+    config.data.signature = signature;
+  }
+
   return config;
 });
 
