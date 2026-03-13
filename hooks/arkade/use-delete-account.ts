6d6bffa18c2e16ac82a4a860867fc9b9a6910411
Michele Guidetti <contact@tkstudio.io>
2026-03-13 14:25:01 +0100

    refactor(account): redirect to home and reset store on account deletion

--- hooks/arkade/use-delete-account.ts
+++ hooks/arkade/use-delete-account.ts
@@ -1,31 +1,37 @@
-import { StorageKeys } from "@/stores/account";
+import useAccountStore, { StorageKeys } from "@/stores/account";
 import { ArkaicAccount } from "@/types/arkaic";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "expo-router";
 import { filter } from "lodash";
 
 export function useDeleteAccount() {
   const queryClient = useQueryClient();
-
+  const router = useRouter();
+  const { setStore } = useAccountStore();
   return useMutation({
     mutationKey: ["delete-account"],
     mutationFn: async (accountName: string) => {
       const storedAccounts = await AsyncStorage.getItem(StorageKeys.Accounts);
-
       const currentAccounts = storedAccounts
         ? (JSON.parse(storedAccounts) as ArkaicAccount[])
         : [];
 
       const newStoredAccounts = filter(
         currentAccounts,
-        (account) => account.name !== accountName
+        (account) => account.name !== accountName,
       );
 
       await AsyncStorage.setItem(
         StorageKeys.Accounts,
-        JSON.stringify(newStoredAccounts)
+        JSON.stringify(newStoredAccounts),
       );
       queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+
+    onSuccess: () => {
+      router.dismissTo("/");
+      setStore({});
     },
   });
 }
