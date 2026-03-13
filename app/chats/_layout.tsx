a5558587ba47c042e1e080f1c65c4e56b87a366d
Michele Guidetti <contact@tkstudio.io>
2026-03-13 16:10:48 +0100

    refactor(chats): simplify layout by removing ScrollView and NavigationMenu

--- app/chats/_layout.tsx
+++ app/chats/_layout.tsx
@@ -1,18 +1,13 @@
-import NavigationMenu from "@/components/navigation-menu";
 import { VStack } from "@/components/ui/vstack";
 
 import { Slot } from "expo-router";
-import { ScrollView } from "react-native";
 
 export default function ProductsLayout() {
   return (
     <VStack className='h-full' space={"lg"}>
-      <ScrollView className='h-full rounded-arkaic-card relative'>
-        <VStack className='h-full' space={"lg"}>
-          <Slot />
-        </VStack>
-      </ScrollView>
-      <NavigationMenu />
+      <VStack className='h-full' space={"lg"}>
+        <Slot />
+      </VStack>
     </VStack>
   );
 }
