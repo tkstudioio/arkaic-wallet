79c5f0982533f8b79eb54d47c9014e2c668b5e1c
Michele Guidetti <contact@tkstudio.io>
2026-03-15 23:00:38 +0100

    fix(navigation): cast route paths as Href for type safety

--- components/create-account.tsx
+++ components/create-account.tsx
@@ -1,4 +1,4 @@
-import { useRouter } from "expo-router";
+import { Href, useRouter } from "expo-router";
 import React from "react";
 import { Button, ButtonText } from "./ui/button";
 import { VStack } from "./ui/vstack";
@@ -8,12 +8,12 @@ export function CreateAccount() {
 
   return (
     <VStack className='w-full' space={"md"}>
-      <Button size='lg' onPress={() => router.push("/account/create")}>
+      <Button size='lg' onPress={() => router.push("/account/create" as Href)}>
         <ButtonText>Create account</ButtonText>
       </Button>
       <Button
         size='lg'
-        onPress={() => router.push("/account/restore")}
+        onPress={() => router.push("/account/restore" as Href)}
         variant={"outline"}
       >
         <ButtonText>Restore account</ButtonText>
