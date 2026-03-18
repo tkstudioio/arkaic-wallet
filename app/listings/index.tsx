f2d59b4d34601bc6e7959416d7324644dbed1084
Michele Guidetti <contact@tkstudio.io>
2026-03-18 16:06:44 +0100

    refactor(listings): streamline layout structure and navigation

--- app/listings/index.tsx
+++ app/listings/index.tsx
@@ -1,8 +1,10 @@
 import { ListingItem } from "@/components/listing-item";
+import NavigationMenu from "@/components/navigation-menu";
 import { Button, ButtonText } from "@/components/ui/button";
 import { HStack } from "@/components/ui/hstack";
 import { Spinner } from "@/components/ui/spinner";
 import { H1, P } from "@/components/ui/typography";
+import { VStack } from "@/components/ui/vstack";
 import { useCategories } from "@/hooks/categories/use-categories";
 import { useListings } from "@/hooks/listings/use-listings";
 import { map } from "lodash";
@@ -20,64 +22,67 @@ export default function ProductsList() {
 
   const filteredListings = selectedCategorySlug
     ? productsQuery.data?.filter((listing) => {
-        console.log(listing.categoryId);
         return listing.category?.slug === selectedCategorySlug;
       })
     : productsQuery.data;
 
-  console.log("prova");
   return (
-    <>
-      <H1 className='font-heading'>Listings</H1>
+    <VStack className='h-full'>
+      <ScrollView className='flex-1'>
+        <VStack space='lg'>
+          <H1 className='font-heading'>Listings</H1>
 
-      <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        className='mb-2'
-      >
-        {match(categoriesQuery)
-          .with({ isLoading: true }, () => null)
-          .otherwise(({ data }) => (
-            <HStack space={"md"}>
-              <Button
-                action={!selectedCategorySlug ? "primary" : "neutral"}
-                onPress={() => setSelectedCategorySlug(null)}
-                variant={"outline"}
-                size={"sm"}
-                className='w-max'
-              >
-                <ButtonText>All</ButtonText>
-              </Button>
-              {map(data, (category) => (
-                <Button
-                  action={
-                    category.slug === selectedCategorySlug
-                      ? "primary"
-                      : "neutral"
-                  }
-                  onPress={() => setSelectedCategorySlug(category.slug)}
-                  variant={"outline"}
-                  key={category.slug}
-                  size={"sm"}
-                  className='w-max'
-                >
-                  <ButtonText>{category.name}</ButtonText>
-                </Button>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className='mb-2'
+          >
+            {match(categoriesQuery)
+              .with({ isLoading: true }, () => null)
+              .otherwise(({ data }) => (
+                <HStack space={"md"}>
+                  <Button
+                    action={!selectedCategorySlug ? "primary" : "neutral"}
+                    onPress={() => setSelectedCategorySlug(null)}
+                    variant={"outline"}
+                    size={"sm"}
+                    className='w-max'
+                  >
+                    <ButtonText>All</ButtonText>
+                  </Button>
+                  {map(data, (category) => (
+                    <Button
+                      action={
+                        category.slug === selectedCategorySlug
+                          ? "primary"
+                          : "neutral"
+                      }
+                      onPress={() => setSelectedCategorySlug(category.slug)}
+                      variant={"outline"}
+                      key={category.slug}
+                      size={"sm"}
+                      className='w-max'
+                    >
+                      <ButtonText>{category.name}</ButtonText>
+                    </Button>
+                  ))}
+                </HStack>
               ))}
-            </HStack>
-          ))}
-      </ScrollView>
+          </ScrollView>
 
-      {match(productsQuery)
-        .with({ isLoading: true }, () => <Spinner className='mt-4' />)
-        .with({ isError: true }, () => (
-          <P className='text-arkaic-negative'>Failed to load products.</P>
-        ))
-        .otherwise(() =>
-          map(filteredListings, (listing) => (
-            <ListingItem listing={listing} key={listing.id} />
-          )),
-        )}
-    </>
+          {match(productsQuery)
+            .with({ isLoading: true }, () => <Spinner className='mt-4' />)
+            .with({ isError: true }, () => (
+              <P className='text-arkaic-negative'>Failed to load products.</P>
+            ))
+            .otherwise(() =>
+              map(filteredListings, (listing) => (
+                <ListingItem listing={listing} key={listing.id} />
+              )),
+            )}
+        </VStack>
+      </ScrollView>
+      <NavigationMenu />
+    </VStack>
   );
 }
