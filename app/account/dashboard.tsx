d7aca49b7e960b266a70fa6a2a5af0600e14f3c7
Michele Guidetti <contact@tkstudio.io>
2026-03-20 14:39:28 +0100

    refactor(account): simplify dashboard layout

--- app/account/dashboard.tsx
+++ app/account/dashboard.tsx
@@ -2,7 +2,6 @@ import { AccountBalance } from "@/components/account-balance";
 import { ReceiveActionSheet } from "@/components/receive-action-sheet";
 import { SendActionSheet } from "@/components/send-action-sheet";
 import { Transactions } from "@/components/transactions";
-import { Card } from "@/components/ui/card";
 import { HStack } from "@/components/ui/hstack";
 import { H1 } from "@/components/ui/typography";
 import React from "react";
@@ -13,16 +12,15 @@ const DashboardPage = () => {
     <>
       <H1>Dashboard</H1>
       <AccountBalance />
-      <Card variant={"ghost"}>
-        <HStack className='w-full' space={"md"}>
-          <View className='flex-1'>
-            <ReceiveActionSheet />
-          </View>
-          <View className='flex-1'>
-            <SendActionSheet />
-          </View>
-        </HStack>
-      </Card>
+
+      <HStack className='w-full' space={"md"}>
+        <View className='flex-1'>
+          <SendActionSheet />
+        </View>
+        <View className='flex-1'>
+          <ReceiveActionSheet />
+        </View>
+      </HStack>
       <Transactions />
     </>
   );
