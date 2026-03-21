dd10f87622529a96cc47fa3a405304504d9979bc
Michele Guidetti <contact@tkstudio.io>
2026-03-21 18:24:46 +0100

    refactor(account): remove ScrollView wrapper and simplify account layout

--- app/account/_layout.tsx
+++ app/account/_layout.tsx
@@ -1,16 +1,13 @@
 import NavigationMenu from "@/components/navigation-menu";
 import { VStack } from "@/components/ui/vstack";
 import { Slot } from "expo-router";
-import { ScrollView } from "react-native";
 
 export default function AccountLayout() {
   return (
     <VStack className='h-full' space={"lg"}>
-      <ScrollView className='h-full rounded-arkaic-card relative'>
-        <VStack className='h-full' space={"lg"}>
-          <Slot />
-        </VStack>
-      </ScrollView>
+      <VStack className='flex-1'>
+        <Slot />
+      </VStack>
       <NavigationMenu />
     </VStack>
   );
